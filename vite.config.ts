933fc222231844d1bf4fd790aeddcf1043e9e3bd
Patrizio Milione <patriziomilione@gmail.com>
2024-05-19 16:20:26 +0200

    2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/patriziomadferitmilione/portfolio_webapp/' 
+  base: '/portfolio_webapp/' 
 });
